b6c92a67cdb8965989f2fe2f33a72b5e45d27629
philxia <phil.xia@gmail.com>
2015-08-24 14:30:14 +0800

    inital submission.

---
+++ ImageEditor/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Telerik.Windows.Media.Imaging.FormatProviders;
+
+namespace ImageEditor
+{
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class MainWindow : Window
+   {
+      public MainWindow()
+      {
+         InitializeComponent();
+
+         this.ImageEditorUI.ImageEditorLoaded += ImageEditorUI_ImageEditorLoaded;
+      }
+
+      void ImageEditorUI_ImageEditorLoaded(object sender, EventArgs e)
+      {
+         string[] args = Environment.GetCommandLineArgs();
+         if (args.Length > 1)
+         {
+            string imagePath = args[1];
+            if (File.Exists(imagePath))
+            {
+               FileInfo fi = new FileInfo(imagePath);
+               string extension = fi.Extension.ToLower();
+               FileStream stream = fi.Open(FileMode.Open);
+
+               IImageFormatProvider formatProvider = ImageFormatProviderManager.GetFormatProviderByExtension(extension);
+               if (formatProvider == null)
+               {
+                  StringBuilder sb = new StringBuilder();
+                  sb.Append("Unable to find format provider for extension: ")
+                    .Append(extension).Append(" .");
+                  return;
+               }
+               else
+               {
+                  this.ImageEditorUI.Image = formatProvider.Import(stream);
+                  this.ImageEditorUI.ImageEditor.ScaleFactor = 0;
+               }
+
+            }
+         }
+      }
+   }
+}
---
+++ ImageEditor/Shape/Arrow.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using Telerik.Windows.Media.Imaging.Shapes;
+
+namespace ImageEditor.Shape
+{
+   public class Arrow : IShape
+   {
+      public string DisplayName
+      {
+         get { return "Arrow"; }
+      }
+
+      public System.Windows.Media.Geometry GetShapeGeometry()
+      {
+         PathFigure outer = new PathFigure();
+         outer.IsClosed = true;
+         outer.StartPoint = new Point(0, 0);
+         outer.Segments.Add(new LineSegment() { Point = new Point(0.82, 0.78) });
+         outer.Segments.Add(new LineSegment() { Point = new Point(0.8, 0.64) });
+         outer.Segments.Add(new LineSegment() { Point = new Point(1, 1) });
+         outer.Segments.Add(new LineSegment() { Point = new Point(0.64, 0.8) });
+         outer.Segments.Add(new LineSegment() { Point = new Point(0.78, 0.82) });
+         outer.Segments.Add(new LineSegment() { Point = new Point(0, 0) });
+
+         
+         PathGeometry logoGeometry = new PathGeometry();
+         logoGeometry.Figures.Add(outer);
+
+         return logoGeometry;
+
+      }
+   }
+}
